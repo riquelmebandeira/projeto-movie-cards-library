f4bb4bcf84ae230fbc9d21b5a0b784d2511cacd4
Riquelme <riquelmebandeira1@outlook.com>
2021-09-08 16:39:52 -0300

    Requisito 8: concluido

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     subtitle: '',
-  //     title: '',
-  //     imagePath: '',
-  //     storyline: '',
-  //     rating: 0,
-  //     genre: 'action',
-  //   };
-  // }
+  constructor() {
+    super();
+    this.state = {
+      // subtitle: '',
+      title: '',
+      // imagePath: '',
+      // storyline: '',
+      // rating: 0,
+      // genre: 'action',
+    };
+
+    this.handleTitleInput = this.handleTitleInput.bind(this);
+  }
+
+  handleTitleInput(event) {
+    this.setState(
+      { [event.target.name]: event.target.value },
+    );
+  }
 
   render() {
     // const { onClick } = this.props;
+    const { title } = this.state;
     return (
-      <form data-testid="add-movie-form" />
+      <form data-testid="add-movie-form">
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            id="title-input"
+            value={ title }
+            data-testid="title-input"
+            onChange={ this.handleTitleInput }
+          />
+        </label>
+      </form>
     );
   }
 }
