16f88827a28085af0291c091d5719a70ca0cba1b
Riquelme <riquelmebandeira1@outlook.com>
2021-09-09 01:30:49 -0300

    Requisitos 16 e 18: concluidos

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
-    // const { movies } = this.props;
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      // movies: { movies },
+      movies: props.movies,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.filterMovieList = this.filterMovieList.bind(this);
   }
 
   handleChange({ target }) {
@@ -23,23 +24,50 @@ class MovieLibrary extends React.Component {
     );
   }
 
+  filterMovieList() {
+    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
+    let result = movies;
+
+    if (bookmarkedOnly === true) {
+      result = movies.filter(({ bookmarked }) => bookmarked === true);
+      return result;
+    }
+    if (selectedGenre !== '') {
+      result = movies.filter(({ genre }) => genre === selectedGenre);
+      return result;
+    }
+    if (searchText !== '') {
+      result = movies.filter(
+        ({ title, subtitle, storyline }) => title.includes(searchText)
+        || subtitle.includes(searchText)
+        || storyline.includes(searchText),
+      );
+      return result;
+    }
+    return result;
+  }
+
   render() {
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const filteredMovieList = this.filterMovieList();
     return (
-      <SearchBar
-        searchText={ searchText }
-        onSearchTextChange={ this.handleChange }
-        bookmarkedOnly={ bookmarkedOnly }
-        onBookmarkedChange={ this.handleChange }
-        selectedGenre={ selectedGenre }
-        onSelectedGenreChange={ this.handleChange }
-      />
+      <>
+        <SearchBar
+          searchText={ searchText }
+          onSearchTextChange={ this.handleChange }
+          bookmarkedOnly={ bookmarkedOnly }
+          onBookmarkedChange={ this.handleChange }
+          selectedGenre={ selectedGenre }
+          onSelectedGenreChange={ this.handleChange }
+        />
+        <MovieList movies={ filteredMovieList } />
+      </>
     );
   }
 }
 
-// MovieLibrary.propTypes = {
-//   movies: PropTypes.array,
-// }.isRequired;
+MovieLibrary.propTypes = {
+  movies: PropTypes.array,
+}.isRequired;
 
 export default MovieLibrary;
